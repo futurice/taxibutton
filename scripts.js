145c91b04eac0fcd55ed24a02852140abe82ab7b
hhol <none@none>
2011-10-25 11:47:28 +0300

    Javascript for TaxiButton(tm)

--- scripts.js
+++ scripts.js
@@ -21,6 +21,7 @@
 
     // Set this to true, if this display has a TaxiButton(tm)
     var TAXI_BUTTON = false;
+    var LAST_UPDATE = Math.round(new Date().getTime()/1000); //Last TaxiUpdate, in seconds since epoch
     
     var now = new Date();
     
@@ -252,41 +253,88 @@
 	resetTaxi();
 
 	if(TAXI_BUTTON){
-	    $('#prefer').live('click', function() {
-		if(!$('.orders').is(':visible')){
-		    $('.no_orders').hide();
-		    $('#taxi').animate({'background-color':'#FFCC00 !important'},350);
-		}
-		$('.orders').append('<p class="taxi_wrap processing">Processing<span class="dot0">.</span><span class="dot1">.</span><span class="dot2">.</span></p>');
-		$('.orders').fadeIn();
-		progressDots(0);
-	    });
-
-	    $('#taxi .processing').live('click', function() {
-		$(this).addClass('response');
-		$(this).append('<br/>Order #'+Math.floor(Math.random()*1000));
-	    });
-
-	    $('#taxi .response').live('click', function() {
-		var rand = Math.floor(Math.random()*5);
-		$(this).removeClass('response');
-		if (rand == 0) {
-		    //fail
-		    $(this).addClass('fail');
-		    $(this).html('Taxi order failed! :(');
-		}else{
-		    //ok
-		    $(this).addClass('success');
-		    $(this).html('Taxi #'+Math.floor(Math.random()*1000)+' ordered!');
-		}
-		var that = this;
-		setTimeout(function(){
-		    $(that).fadeOut(250);
-		    setTimeout(function(){ $(that).remove(); }, 300);
-    setTimeout(function(){ if($('.taxi_wrap').length==0) { $('.orders').hide(); $('.no_orders').fadeIn(250); $('#taxi').animate({'background-color':'#4C9018 !important'},500); }}, 500);
-		},3000);
-	    });
+	    setInterval(updateTaxiInformation,500);
 	}
 
     });
+
+    function updateTaxiInformation(){
+	$.get('messages.txt',function(data){
+	    var lines = data.split("\n");
+	    lines.pop(); //remove the extra line
+	    for(var i in lines){
+		var line = lines[i];
+		var parts = line.split(":");
+		var timestamp = parts[0];
+		var key = parts[1].split(" ")[0];
+		var value = parts[1].split(" ")[1];
+		if(timestamp > LAST_UPDATE){
+		    LAST_UPDATE = timestamp;
+		    if(key == "CLICK"){
+			if(!$('.orders').is(':visible')){
+			    $('.no_orders').hide();
+			    $('#taxi').animate({'background-color':'#FFCC00 !important'},350);
+			}
+			$('.orders').append('<p class="taxi_wrap processing">Processing<span class="dot0">.</span><span class="dot1">.</span><span class="dot2">.</span></p>');
+			$('.orders').fadeIn();
+			progressDots(0);
+			setTimeout(removeFirstProcessing, 120000);
+		    }else if(key == "TILAUS"){
+			removeFirstProcessing("no_reset");
+			$('.orders').append('<p class="taxi_wrap response">Waiting for taxi confirmation (order #'+value+')</p>');
+			setTimeout(removeFirstResponse, 60000);
+		    }else if(key == "TAKSI"){
+			removeFirstResponse("no_reset");
+			$('.orders').append('<p class="taxi_wrap success">Taxi #'+value+' has confirmed your order! \\o/</p>');
+			setTimeout(removeFirstSuccess, 60000);
+		    }else if(key == "VARATTU"){
+			removeFirstResponse("no_reset");
+			$('.orders').append('<p class="taxi_wrap fail">There\'s no taxis available at the moment, please try again shortly.</p>');
+			setTimeout(removeFirstFail, 10000);
+		    }else if(key == "ERROR"){
+			removeFirstResponse("no_reset");
+			$('.orders').append('<p class="taxi_wrap fail">An error happened, try again.</p>');
+			setTimeout(removeFirstFail, 10000);
+		    }
+		}
+	    }
+	});
+    }
+
+    function removeFirstFail(reset){
+	$('.taxi_wrap.fail').first().remove();
+	if(reset != "no_reset"){
+	    resetTaxiState();
+	}
+    }
+
+    function removeFirstSuccess(reset){
+	$('.taxi_wrap.success').first().remove();
+	if(reset != "no_reset"){
+	    resetTaxiState();
+	}
+    }
+
+    function removeFirstResponse(reset){
+	$('.taxi_wrap.response').first().remove();
+	if(reset != "no_reset"){
+	    resetTaxiState();
+	}
+    }
+
+    function removeFirstProcessing(reset){
+	$('.taxi_wrap.processing').first().remove();
+	if(reset != "no_reset"){
+	    resetTaxiState();
+	}
+    }
+
+    function resetTaxiState(){
+	if($('.taxi_wrap').length==0) {
+	    $('.orders').hide();
+	    $('.no_orders').fadeIn(250);
+	    $('#taxi').animate({'background-color':'#4C9018 !important'},500);
+	}
+    }
+
 }(jQuery));
