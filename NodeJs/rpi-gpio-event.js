32bf4b0f5aa56a4aadde135e3f5907a700099a6c
Alex Matrosov <alex.matrosov@futurice.com>
2015-05-26 13:41:15 +0300

    Added package.json

--- NodeJs/rpi-gpio-event.js
+++
@@ -1,7 +0,0 @@
-var gpio = require('rpi-gpio');
-
-gpio.on('change', function(channel, value) {
-    console.log('Channel ' + channel + ' value is now ' + value);
-});
-
-gpio.setup(18, gpio.DIR_IN);
