4b8156afd73199e150385a480c772bd30f7632c6
Alex Matrosov <alex.matrosov@futurice.com>
2015-06-23 18:54:36 +0300

    Implemented weekdays and ticker line

--- NodeJs/public/js/index/calendar.js
+++ NodeJs/public/js/index/calendar.js
@@ -4,10 +4,12 @@
         var config;
         var weatherRefreshTimeoutObject;
         var timeRefreshTimeoutObject;
+        var thisPlaceLastNow;
         var $calendar;
         var $ticker;
         var dayNumberTemplate;
         var thisPlaceWeatherTemplate;
+        var tickerTemplate;
         var tickerItemTemplate;
         
         var formatTemperature = function(temperature) {
@@ -20,14 +22,33 @@
             $calendar.find('.this-place .name').text(thisPlace.name);
 
             var restPlaces = _.rest(config.places, 1);
-            var tickerItemsData = _.map(restPlaces, function(place){
+            var tickerItemsData = _.map(restPlaces, function(place, index){
                 return {
+                    index: index,
                     name: place.name,
                     time: '00:00',
                     temperature: 0,
                 };
             });
-            $ticker.html(futu.templates.renderMany(tickerItemTemplate, tickerItemsData, '&nbsp;/&nbsp;'));
+            var lineHtml = futu.templates.renderMany(tickerItemTemplate, tickerItemsData, '&nbsp;&nbsp;/&nbsp;&nbsp;');
+            $ticker.html(futu.templates.render(tickerTemplate, {lineHtml: lineHtml}));
+        };
+
+        var renderDayNumbers = function(now){
+            var dayMoments = _.map(_.range(0, 7), function(offset){
+                var cloneNow = moment(now);
+                return cloneNow.add(offset, 'd');
+            });
+            var templateData = _.map(dayMoments, function (m, index) {
+                var isoWeekday = m.isoWeekday();
+                return {
+                    dayOfMonth: m.format('DD'),
+                    dayOfWeek: index == 0 ? m.format('ddd') : '',
+                    weekendClass: isoWeekday == 6 || isoWeekday == 7 ? 'weekend' : ''
+                };
+            });
+            var html = futu.templates.renderMany(dayNumberTemplate, templateData);
+            $calendar.find('.weekdays').html(html);
         };
 
         var timeRefresh = function() {
@@ -35,11 +56,17 @@
             var format = 'HH:mm';
 
             var thisPlace = _.first(config.places);
-            $calendar.find('.current-time').text(now.tz(thisPlace.timezone).format(format));
+            var thisPlaceNow = now.tz(thisPlace.timezone);
+            $calendar.find('.current-time').text(thisPlaceNow.format(format));
+            if(thisPlaceNow.date() != thisPlaceLastNow.date())
+            {
+                renderDayNumbers(thisPlaceNow);
+            }
+            thisPlaceLastNow = thisPlaceNow;
 
             var restPlaces = _.rest(config.places, 1);
             _.each(restPlaces, function(place, index) {
-                $ticker.find('span:nth-of-type(' + (index + 1) + ') .time').text(now.tz(place.timezone).format(format));
+                $ticker.find('span.place-' + index + ' .time').text(now.tz(place.timezone).format(format));
             });
         };
 
@@ -62,7 +89,7 @@
 
                 var restResults = _.rest(results, 1);
                 _.each(restResults, function(result, index) {
-                    $ticker.find('span:nth-of-type(' + (index + 1) + ') .temperature').text(formatTemperature(result.main.temp));
+                    $ticker.find('span.place-' + index + ' .temperature').text(formatTemperature(result.main.temp));
                 });
             });
         };
@@ -76,7 +103,9 @@
                     $ticker = $('#calendar .ticker');
                     dayNumberTemplate = futu.templates.find('#calendar-day-number-template');
                     thisPlaceWeatherTemplate = futu.templates.find('#calendar-this-place-weather-template');
+                    tickerTemplate = futu.templates.find('#calendar-ticker-template');
                     tickerItemTemplate = futu.templates.find('#calendar-ticker-item-template');
+                    thisPlaceLastNow = moment(0);
 
                     renderCalendar();
 
