c87fd725c92f855f27a476c4bce1aa01b0decb46
Topi Paavilainen <topi.paavilainen@futurice.com>
2016-06-02 13:24:16 +0300

    api keys, usernames and passwords removed from git history

---
+++ NodeJs/config.js
@@ -0,0 +1,89 @@
+module.exports = {
+    "logging": {
+        "level": "INFO" // DEBUG | INFO | NOTICE | WARNING | ERROR | CRITICAL
+    },
+    "httpServer" : {
+        "port": 8080
+    },
+    "webSocketServer": {
+        "port": 8081
+    },
+    "smsGate": {
+        "isFaked": true, // Enable to prevent SMS being really sent
+        "host": "localhost",
+        "port": 13013,
+        "path": "/cgi-bin/sendsms",
+    },
+    "taxiService": {
+        "phoneNumber": "13170", // Taxi Helsinki number - 13170
+        "orderMessage": "Helsinki Annankatu 34 B  Futurice Oy"
+    },
+    "button":  {
+        "pin": 18, // GPIO pin number, not connector pin number
+        "debounceTimeout": 120, // ms
+    },
+    "taxiMachine": {
+        "pressedTimeout": 3 * 1000, // ms
+        "quickieTimeout": 5 * 1000, // ms
+        "orderingTaxiTimeout": 8 * 60 * 1000, // ms. Should be 8 min according to Taxi Helsinki
+        "awaitingTaxiTimeout": 8 * 60 * 1000, // ms. Should be 8 min according to Taxi Helsinki
+        "taxiConfirmedTimeout": 30 * 1000, // ms
+        "allBusyTimeout": 20 * 1000,// ms
+        "orderFailedTimeout": 20 * 1000 // ms
+    },
+    "calendar": {
+        "weather": { // http://openweathermap.org/
+            "apiUrl": "http://api.openweathermap.org/data/2.5/weather",
+            "refreshInterval": 5 * 60 * 1000 // ms
+        },
+        "places": [
+            {
+                "name": "Helsinki",
+                "query": "Helsinki,FI",
+                "timezone": "Europe/Helsinki",
+            },
+            {
+                "name": "Tampere",
+                "query": "Tampere,FI",
+                "timezone": "Europe/Helsinki",
+            },
+            {
+                "name": "Berlin",
+                "query": "Berlin,DE",
+                "timezone": "Europe/Berlin",
+            },
+            {
+                "name": "London",
+                "query": "London,UK",
+                "timezone": "Europe/London",
+            },
+            {
+                "name": "Stockholm",
+                "query": "Stockholm, SE",
+                "timezone": "Europe/Stockholm",
+            },
+            {
+                "name": "München",
+                "query": "Muenchen,DE",
+                "timezone": "Europe/Berlin",
+            },
+            // {
+            //     "name": "Helsinki",
+            //     "query": "Helsinki,FI", // Query to request weather data from http://openweathermap.org/
+            //     "timezone": "Europe/Helsinki", // Timezone name according to http://momentjs.com/timezone/
+            // },
+        ]
+    },
+    "schedule": {
+        "refreshInterval": 60 * 60 * 1000, // ms
+        "removePastDepaturesInterval": 6 * 1000, // ms
+        "switchStopsInterval": 8 * 1000, // ms
+        "apiUrl": "http://api.reittiopas.fi/hsl/prod/",
+        "stopCodes": [
+            "1237", // Kamppi(M), Laituri 76 (1237)
+            "1222", // Kamppi(M), Laituri 75 (1222)
+            "0231", // Simonkatu (0231)
+            "0232", // Simonkatu (0232)
+        ],
+    }
+};
---
+++ NodeJs/secrets.js
@@ -0,0 +1,17 @@
+module.exports = {
+	"smsGate": {
+    	"credentials": {
+        	"username": "KANNEL_USERNAME",
+        	"password": "KANNEL_PASSWORD"
+    	}
+	},
+    "calendar": {
+        "weather": { // http://openweathermap.org/
+            "apikey": "OPENWEATHERMAP_API_KEY", // alexei.matrosov@futurice.com / 31337
+        }
+    },
+    "schedule": {
+        "username": "REITTIOPAS_API_USERNAME",
+        "password": "REITTIOPAS_API_PASSWORD",
+    }
+}
